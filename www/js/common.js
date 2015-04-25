490afba9b45632214c2ec9243266bb13316ef3bf
Alexander Schrab <alexander.schrab@gmail.com>
2015-04-25 20:50:57 +0200

    add socket.io to git

--- www/js/common.js
+++ www/js/common.js
@@ -7,7 +7,7 @@ requirejs.config({
     },
     "paths": {
         "jquery": "jquery-2.1.3.min",
-        "socketio": "https://cdn.socket.io/socket.io-1.3.5",
+        "socketio": "socket.io-1.3.5",
         "howler": "howler.min",
         "apps": "../apps"
     }
