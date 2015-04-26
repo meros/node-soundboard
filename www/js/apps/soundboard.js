163ce1babc23cc50aac42cc7d7484b6c17b4f5f5
Alexander Schrab <alexander.schrab@gmail.com>
2015-04-26 11:47:42 +0200

    working remote/broadcast/local with three-way switch

--- www/js/apps/soundboard.js
+++ www/js/apps/soundboard.js
@@ -1,34 +1,49 @@
 define(["jquery", "socketio", "howler"], function($, io, howler) {
     var socket = io();
-
     // Function returning true if sound should be played remote
     var playRemote = function() {
         return $('.switch-play-remote').prop('checked');
     }
-
-    // Play sound remote or in browser
-    var playSound = function(id, playRemote) {
-        if (playRemote) {
-            $.ajax({
-                url: '?id=' + id
-            });
-        } else {
-            socket.emit('play', id);
+    var getTarget = function() {
+            return $('.list-group-item.active').attr('data-target');
         }
-    }
-
-    // Bind click on all sound buttons
-    $(".button-sound").each(function(_, sound) {
-        var soundfile = $(sound).attr("data-sound");
-        $(sound).click(function() {
-            playSound(soundfile, playRemote());
+        // Play sound remote or in browser
+    var playSoundRemote = function(soundfile) {
+        $.ajax({
+            url: '?id=' + soundfile
         });
+    };
+    // Play sound broadcast
+    var playSoundBroadcast = function(soundfile) {
+            socket.emit('play', soundfile);
+        }
+        // Play sound local
+    var playSoundLocal = function(soundfile) {
+            new howler.Howl({
+                urls: ['/data/' + soundfile]
+            }).play();
+        }
+        // User clicked on sound button
+    $(document).on("click", ".button-sound", function() {
+        var soundfile = $(this).attr("data-sound");
+        var target = getTarget();
+        if (target === "remote") {
+            playSoundRemote(soundfile);
+        } else if (target === "broadcast") {
+            playSoundBroadcast(soundfile);
+        } else if (target === "local") {
+            playSoundLocal(soundfile);
+        }
+    });
+    // Server gives us a play direction
+    socket.on('play', function(soundfile) {
+        playSoundLocal(soundfile);
     });
 
-    socket.on('play', function(soundfile) {
-        new howler.Howl({
-            urls: ['/data/' + soundfile]
-        }).play();
+    $('.list-group-item').on('click', function(e) {
+        $(this).closest(".list-group").children().removeClass('active');
+        $(this).addClass('active');
 
+        return false;
     });
 });
